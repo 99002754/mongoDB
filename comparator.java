4c41fe6f0346ed8d21e4fe844369534ceb79eac5
99002754 <72496632+99002754@users.noreply.github.com>
2020-10-20 09:36:36 +0530

    Update comparator.java

--- comparator.java
+++ comparator.java
@@ -5,11 +5,11 @@ public class Main {
   public static void main(String[] args) {
   
     ArrayList list = new ArrayList();
-    list.add("Sandeep");
     list.add("Yash");
-    list.add("Teja");
-    list.add("Trinath");
-    list.add("Ganesh");
+    list.add("Harsh");
+    list.add("Akshay");
+    list.add("Tony");
+    list.add("Prafful");
     System.out.println("Elements in ArrayList prior sorting :");
     for(int i=0; i < list.size(); i++)
  
